a72efa9094dcd379d38e5187a388d80287a3ce7f
ale2006vargas <alejndravargas401@gmail.com>
2025-01-24 18:19:32 -0300

    Final Version Game "Amigo Secreto"

---
+++ app.js
@@ -0,0 +1,48 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array para almacenar los nombres
+let amigos = [];
+
+// Función para agregar un nombre a la lista
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    } else if (amigos.includes(nombre)) {
+        alert('Este amigo ya fue agregado');
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = '';
+    actualizarListaAmigos();
+}
+
+// Función para actualizar la lista de nombres en el HTML
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+// Función para sortear un amigo secreto
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista para sortear.');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+}
